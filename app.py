990ed089405c8ad9ad20bc0cf41601afbb9e658d
Sree Kanikadevi <sreekanikadevik.cse2024@citchennai.net>
2026-06-12 00:06:52 +0530

    Added Brevo debug logs

--- app.py
+++ app.py
@@ -59,21 +59,23 @@ def send_email(receiver_email, subject, body):
 
         msg.set_content(body)
 
+        print("BREVO LOGIN =", BREVO_LOGIN)
+        print("BREVO KEY EXISTS =", BREVO_SMTP_KEY is not None)
+
         with smtplib.SMTP(
-            "smtp-relay.brevo.com",
+           "smtp-relay.brevo.com",
             587,
-            timeout=20
+            timeout=30
         ) as smtp:
 
-            smtp.starttls()
+        smtp.starttls()
 
-            smtp.login(
-                BREVO_LOGIN,
-                BREVO_SMTP_KEY
-            )
+        smtp.login(
+             BREVO_LOGIN,
+             BREVO_SMTP_KEY
+    )
 
-            smtp.send_message(msg)
-
+        smtp.send_message(msg)
         return True
 
     except Exception as e:
